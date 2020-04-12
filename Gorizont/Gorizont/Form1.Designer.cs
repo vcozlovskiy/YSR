d6d269783707cd2dd5e4763f211b7b2d76caeba9
vcozlovskiy <38006998+vcozlovskiy@users.noreply.github.com>
2020-04-12 11:01:15 +0300

    Add files via upload

---
+++ Gorizont/Gorizont/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Gorizont
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.axis1 = new myUCLib.Axis();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Скорость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Угол";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(88, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 48);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Без сопротивления";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseClick);
+            // 
+            // axis1
+            // 
+            this.axis1.axis_bkcolor = System.Drawing.Color.White;
+            this.axis1.axis_color = System.Drawing.Color.Gray;
+            this.axis1.Axis_Type = ((byte)(1));
+            this.axis1.E_x = 0;
+            this.axis1.E_y = 0;
+            this.axis1.Location = new System.Drawing.Point(16, 159);
+            this.axis1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.axis1.Name = "axis1";
+            this.axis1.Pix_color = System.Drawing.Color.Black;
+            this.axis1.Pix_Size = 2F;
+            this.axis1.Pix_type = ((byte)(1));
+            this.axis1.Size = new System.Drawing.Size(771, 358);
+            this.axis1.TabIndex = 6;
+            this.axis1.x_Base = 1F;
+            this.axis1.x_Name = "X";
+            this.axis1.y_Base = 1F;
+            this.axis1.y_Name = "Y";
+            this.axis1.Load += new System.EventHandler(this.Axis1_Load);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 48);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button2_MouseClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "С учетом сопротивления ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Button3_MouseClick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(88, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(359, 44);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(359, 72);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Масса";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(359, 13);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(336, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(336, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(336, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "T";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.axis1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private myUCLib.Axis axis1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Gorizont/Gorizont/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace Gorizont
+{
+    public partial class Form1 : Form
+    {
+        float x=0,x1 = 1, y=0, t = 0, ux =10, uy =10, g = 9.81f,a =-1;
+        float f = 0, av = 0,k=0.01f, m = 1;
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            
+            t = t + 0.1f;
+            ux = ux - 0.1f * ux * ux * k/m;
+            x = x + ux * 0.1f;
+            uy = uy + 0.01f * g;
+            y = y - uy * 0.1f;
+            textBox6.Text = Convert.ToString(x);
+            textBox4.Text = Convert.ToString(y);
+            textBox5.Text = Convert.ToString(t);
+            //x = ux * t - k * ux * ux * t * t / 2;
+            //y = -uy * t - g * t * t / 20;
+            axis1.PixDraw(x, y, Color.Blue, 1);
+            if (y <= 0)
+            {
+                timer2.Stop();
+                x = 0; y = 0; t = 0;
+            }
+           
+           axis1.StatToPic();
+        }
+
+        private void Button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            a = (float)Convert.ToDouble(textBox2.Text);
+            ux = (float)(Convert.ToDouble(textBox1.Text) * Math.Cos(a*Math.PI/180 ));
+            uy = (float)(Convert.ToDouble(textBox1.Text) * Math.Sin(-a*Math.PI/180 ));
+            m = (float)(Convert.ToDouble(textBox3.Text));
+            timer2.Interval = 1;
+            timer2.Start();
+            axis1.Refresh();
+        }
+
+        private void Button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            x = 0; y = 0; t = 0; 
+        }
+
+        private void Axis1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(30);
+            textBox2.Text = Convert.ToString(45);
+            textBox3.Text = Convert.ToString(1);
+            axis1.Axis_Type = 3;
+            axis1.x_Name = "Ox";
+            axis1.y_Name = "Oy";
+            axis1.x_Base = 1000;
+            axis1.y_Base = 1000;
+            axis1.Pix_Size = 0.0001f;
+            axis1.AxisDraw();
+            axis1.PixDraw(x, y, Color.Blue, 1);
+            axis1.StatToPic();
+            axis1.Refresh();
+        }
+
+        private void Button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            a = (float)Convert.ToDouble(textBox2.Text);
+            ux = (float)(Convert.ToDouble(textBox1.Text) * Math.Cos(a * Math.PI / 180));
+            uy = (float)(Convert.ToDouble(textBox1.Text) * Math.Sin(-a * Math.PI / 180));
+            timer1.Interval = 10;
+            timer1.Start();
+            axis1.Refresh();
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            t = t + 0.1f;
+            x = ux * t;
+            y = -uy * t - g * t * t / 20;
+            textBox6.Text = Convert.ToString(x);
+            textBox4.Text = Convert.ToString(y);
+            textBox5.Text = Convert.ToString(t);
+            if (y <= 0)
+            {
+                timer1.Stop();
+                x = 0; y = 0; t = 0;
+            }
+            axis1.PixDraw(x, y, Color.Red, 1);
+            axis1.StatToPic();
+        }
+    }
+}
